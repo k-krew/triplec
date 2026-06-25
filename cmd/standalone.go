f3d2cfa2e5aebf17c7fef13b22d4b1f03ba4fa2c
kreicer <listopadovdv@gmail.com>
2026-06-25 22:54:17 +0200

    feat: Standalone mode is fully functional for local certificate management

--- cmd/standalone.go
+++ cmd/standalone.go
@@ -1,13 +1,25 @@
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/kreicer/triplec/pkg/config"
 	"github.com/kreicer/triplec/pkg/updater"
 )
 
 func runStandalone(cfg *config.Config, save updater.SaveFunc) error {
-	_ = save
-	return fmt.Errorf("standalone mode: not yet implemented")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	slog.Info("starting standalone mode")
+
+	u := updater.New(cfg, save)
+	u.Start(ctx)
+
+	slog.Info("shutdown complete")
+	return nil
 }
